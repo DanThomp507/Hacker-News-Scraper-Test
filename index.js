35d7baa633f9c424c4cf65917f50f3704a4da92a
Daniel Thompson <DanThomp507@gmail.com>
2019-05-01 13:50:38 -0400

    created instance of axios, grabbed html data, and set an empty array into which the results will be pushed

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const url = "https://news.ycombinator.com/";
+
+// creating an instance of axios and passing it the Hacker News url as an argument
+axios(url).then(response => {
+  // we get the html data
+  const html = response.data;
+  // we load the html data into cheerio
+  const $ = cheerio.load(html);
+
+  //selecting each span element with the class called comhead and setting
+  //that to a variable
+
+  const newsPosts = $("span.comhead");
+
+  // setting an empty array to push the results into
+  const data = [];
+});
